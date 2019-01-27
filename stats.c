324ec0857516910b9de878473ff1ccf8519f964d
Herbert <Brittwear@aol.com>
2019-01-27 14:13:15 -0500

    Adding code outlined and documentation for C1M1 Assessment

--- stats.c
+++ stats.c
@@ -1,4 +1,35 @@
 /******************************************************************************
+ * @file stats.c
+ * @brief File for Intro-Embedded-Systems Peer-graded Assignment 1
+ *
+ * @author Herbert Thompson
+ * @date 2019-1-27
+ *
+ * A simple C-Programming example that exhibits a handful of basic c-programming
+ * features to show how to calculate some statistics on a set of numbers and 
+ * display the information
+ *     -Print the statistics
+ *     -Print the array
+ *     -Find the median
+ *     -Find the mean
+ *     -Find the maximum
+ *     -Find the minimum
+ *     -Sort the array
+ *
+ *****************************************************************************/ 
+
+/****************************************************************************
+* Copyright (C) 2019 by Herbert Thompson 
+*
+* Redistribution, modification or use of this software in source or binary
+* forms is permitted as long as the files maintain this copyright. Users are 
+* permitted to modify this and use it to learn about the field of embedded
+* software. Herbert Thompson is not liable for any
+* misuse of this material.
+* 
+********************************************************************************
+
+/******************************************************************************
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
@@ -8,17 +39,6 @@
  * misuse of this material. 
  *
  *****************************************************************************/
-/**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
- *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
- *
- */
-
 
 
 #include <stdio.h>
@@ -27,17 +47,68 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
+int main() {
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
+ unsigned char test[SIZE] = {  34, 201, 190, 154,   8, 194,   2,   6,
+                              114,  88,  45,  76, 123,  87,  25,  23,
+                              200, 122, 150,  90,  92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+
+ /* First print out the array so user can see it */
+ print_array(test, SIZE);
+
+
+ /* Now print out the sorted array so user can easily refer to values
+  * This part will print out the returned array from sort_array 
+  */
+ sort_array(test, SIZE);
+	
+ /* Finally print out the required statistics for the given array 
+  * This function will call the support functions below */
+ print_statistics();
+ 
+
+ return 0;
 
 }
 
-/* Add other Implementation File Code Here */
+
+ /* Below are the support functions for the above functions */
+
+ /* Print out the given array */
+ void print_array(unsigned char * ptr, unsigned int size){
+	 ;
+ }
+
+ /* Sort the given array from large to small */
+ unsigned char * sort_array(unsigned char * ptr, unsigned int length){
+	 ;
+ }
+
+ /* Print out the statistics for the given array */
+ void print_statistics(void){
+	;
+  }
+
+ /* Find the median vlaue of the given array */
+ unsigned char find_median(unsigned  char * ptr, unsigned int length){
+	;
+ }
+
+ /* Find the mean of the given array */
+ unsigned char find_mean(unsigned  char * ptr, unsigned int length){
+	 ;
+ }
+
+ /* Find the maximum value in the given array */
+ unsigned char find_maximum(unsigned  char * ptr, unsigned int length){
+	 ;
+ }
+
+ /* Find the minumum value in the array */
+ unsigned char find_minimum(unsigned  char * ptr, unsigned int length){
+	 ;
+ }
+
--- stats.h
+++ stats.h
@@ -1,3 +1,14 @@
+/********************************************************************************
+* Copyright (C) 2019 by Herbert Thompson 
+*
+* Redistribution, modification or use of this software in source or binary
+* forms is permitted as long as the files maintain this copyright. Users are 
+* permitted to modify this and use it to learn about the field of embedded
+* software. Herbert Thompson is not liable for any
+* misuse of this material.
+* 
+********************************************************************************/
+
 /******************************************************************************
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
@@ -8,33 +19,113 @@
  * misuse of this material. 
  *
  *****************************************************************************/
+
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.h
+ * @brief Header file for array statistics for assignment 1 for Intro Embedded 
+ *        Systems Course
  *
- * <Add Extended Description Here>
+ * This header file contains the declarations for the required functions
+ * for assignment 1 in the Coursera Introduction Embedded Systems course.
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Herbert Thompson 
+ * @date   January 27 2019
  *
  */
+
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+/**
+ * @brief Prints out the statistics for the supplied array 
+ * 
+ * This function prints out to the screen the required
+ * statictics for assignment 1 supplied array.
+ *
+ * @param void 
+ *
+ * @return void
+ */
+void print_statistics(void);
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief Prints out the array to the screen
+ * 
+ * Given an array and the size of the array as inputs
+ * this function will print this array out to the screen
  *
- * <Add Extended Description Here>
+ * @param ptr Pointer to data array
+ * @param size Length of the  data array
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
+ * @return void
  */
+void print_array(unsigned char * ptr, unsigned int size);
 
+/**
+ * @brief Calculates the median value of the given array 
+ * 
+ * Given an array and the length of the array as inputs
+ * this function will return the median value of the array
+ *
+ * @param ptr Pointer to data array
+ * @param length Length of the array
+ *
+ * @return Median of the array
+ */
+unsigned char find_median(unsigned  char * ptr, unsigned int length);
+
+/**
+ * @brief Calculates the mean value of the given array 
+ * 
+ * Given an array and the length of the array as inputs
+ * this function will return the mean value of the array
+ *
+ * @param ptr Pointer to data array
+ * @param length Length of the array
+ *
+ * @return Mean of the array
+ */
+unsigned char find_mean(unsigned  char * ptr, unsigned int length);
+
+/**
+ * @brief Calculates the maximum value in the given array 
+ * 
+ * Given an array and the length of the array as inputs
+ * this function will return the maximum value in the array
+ *
+ * @param ptr Pointer to data array
+ * @param length Length of the array
+ *
+ * @return Maximum value in the array
+ */
+unsigned char find_maximum(unsigned  char * ptr, unsigned int length);
+
+/**
+ * @brief Calculates the mimimum value in the given array 
+ * 
+ * Given an array and the length of the array as inputs
+ * this function will return the minimum value in the array
+ *
+ * @param ptr Pointer to data array
+ * @param length Length of the array
+ *
+ * @return Minimum value in the array
+ */
+unsigned char find_minimum(unsigned  char * ptr, unsigned int length);
+
+/**
+ * @brief Sorts the given array from largest to smallest value
+ * 
+ * Given an array and the length of the array as inputs
+ * this function will sort the array from largest to smallest value
+ * and return that array
+ *
+ * @param ptr Pointer to data array
+ * @param length Length of the array
+ *
+ * @return prt A pointer to the sorted array
+ */
+unsigned char * sort_array(unsigned char * ptr, unsigned int length);
 
 #endif /* __STATS_H__ */
+
